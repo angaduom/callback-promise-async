db61337b059262a2fac3a58bd871a5a71cb34bca
Angad Singh <Angad Singh>
2016-05-09 12:50:44 -0400

    final touches

--- index.js
+++ index.js
@@ -1,76 +1,92 @@
-
-
-
 var fs = require('fs');
 
 // what is asynchronosity?
+// the first example shows how node is asynchromous.
 
-console.log("Before readFile")
-fs.readFile('text.txt','utf8', function(err, contents) {
-  console.log('Async read file', contents.length);
+console.log("Before fs.readFile")
+fs.readFile('text.txt', 'utf8', function(err, text) {
+    console.log('fs.readFile() ', text.length);
 });
-console.log("After readFile");
+console.log("After fs.readFile");
 
+// lets try using readFileSync, this function will wait for the read to happen
+// and then the process moves on
 
-// lets try using readFileSync
-
-
- console.log("Before readFileSync");
- var text=fs.readFileSync('text.txt','utf-8');
- console.log(text.length);
- console.log("After readFileSync");
+console.log("Before fs.readFileSync");
+var text = fs.readFileSync('text.txt', 'utf-8');
+console.log("fs.readFileSync ", text.length);
+console.log("After fs.readFileSync");
 
 
 //so what is a callback
+function readFileCallback(fileName, callback) {
+    console.log("In the function");
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+        if (err) {
+            callback(err)
+        } else {
+            callback(null, data);
+        }
 
-function readFileCallback(fileName,callback){
-  console.log("In the function");
-  fs.readFile(fileName,'utf-8',(err,data)=>{
-    if(err)
-      console.log(err)
-      console.log(data.length)
-      callback(data);
-  })
-}//readFile
+    })
+} //readFile
 
+
+//here is the callback section
 console.log("Before");
-readFileCallback('text.txt',function(data){
-  console.log(data.length)
+readFileCallback('text.txt', function(err, data) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("Callback function ", data.length)
+    }
+
+
 })
 console.log("After");
 
-new Promise((resolve,reject)=>{
-  fs.readFile('text.txt','utf-8',(err,data)=>{
-    if(err)
-      reject(err)
-    else{
-      //if the data was read correctly
-      resolve(data);
-    }
-  })
-}).then((result)=>{
-  console.log("promise .then()")
-  console.log("Promise result",result.length)
-}).catch(function(err){
-  console.log("catch");
-  console.log(err)
+
+//here is the promises section
+new Promise((resolve, reject) => {
+    fs.readFile('text.txt', 'utf-8', (err, data) => {
+        if (err)
+            reject(err)
+        else {
+            //if the data was read correctly
+            resolve(data);
+        }
+    })
+}).then((result) => {
+    console.log("First then()")
+    console.log("Promise result ", result.length)
+    return result.length;
+}).then((result) => {
+    console.log("Second then()")
+    console.log("Promise result ", result)
+}).catch(function(err) {
+    console.log("catch");
+    console.log(err)
 })
 
 //readFileAsync
-function readFileAsync(){
-  return new Promise(function(resolve, reject){
-    fs.readFile('text.txt','utf-8',(err,data)=>{
-      resolve(data);
+function readFileAsync() {
+    return new Promise(function(resolve, reject) {
+        fs.readFile('text.txt', 'utf-8', (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data);
+            }
+        })
     })
-  })
+}
+
+async function read() {
+    try {
+        var text = await readFileAsync();
+        console.log("Async Await", text.length);
+    } catch (err) {
+        console.log(err);
+    }
 }
 read();
-async function read(){
-  try{
-    var text = await readFileAsync();
-    console.log("Async Await",text.length);
-  }
-  catch(err){
-    console.log(err);
-  }
-}
